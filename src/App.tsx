b4978c761542b8b54a51e694cefaedd9a2a80eae
Nicolas GIRARD <NG38@outlook.fr>
2025-04-09 10:41:59 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import { useState } from 'react'
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
-import './App.css'
+import { Link, Outlet } from "react-router";
+
+import "./App.css";
 
 function App() {
-    const [currentLocation, setCurrentLocation] = useState("/");
-
     return (
         <>
             <nav>
-                <button onClick={() => setCurrentLocation("/")} type="button">
-                    Home
-                </button>
-                <button onClick={() => setCurrentLocation("/about")} type="button">
-                    About
-                </button>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
             </nav>
             <main>
-                {currentLocation === "/" && <Home />}
-                {currentLocation === "/about" && <About />}
+                <Outlet />
             </main>
         </>
     );
 }
 
-export default App
+export default App;
